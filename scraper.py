680223dd64457f81f5d6627d31b981f6a2f7e05f
yuvron <yuvr99@gmail.com>
2022-09-10 21:42:10 +0300

    Added scraping of posts' authors and dates

--- scraper.py
+++ scraper.py
@@ -30,7 +30,35 @@ def get_contents():
     return contents
 
 
+# Scrapes all the posts' information (author and date) from the page
+def get_infos():
+    infos_elements = soup.select(".col-sm-6:nth-child(odd)")
+    infos = [info.getText().strip() for info in infos_elements]
+    return infos
+
+
+# Extracts the author from a post information
+def get_authors(infos):
+    authors = [info.split("by")[1].split("at")[0].strip() for info in infos]
+    return authors
+
+
+# Extracts the date from a post information
+def get_dates(infos):
+    dates = [info.split("at")[1].strip()[:-4] for info in infos]
+    dates = [datetime.strptime(date, "%d %b %Y, %H:%M:%S") for date in dates]
+    return dates
+
+
 titles = get_titles()
 contents = get_contents()
-print(titles)
-print(contents)
+infos = get_infos()
+authors = get_authors(infos)
+dates = get_dates(infos)
+
+# Builds all the posts with title, content, author and date
+posts = []
+for i in range(len(titles)):
+    post = {"title": titles[i], "content": contents[i], "author": authors[i], "date": dates[i]}
+    posts.append(post)
+    print(post)
