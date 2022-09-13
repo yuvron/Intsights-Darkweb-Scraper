9a88c231bcb0f9304978c4b8bf59d07e501ac87b
yuvron <yuvr99@gmail.com>
2022-09-13 11:37:49 +0300

    Added basic api logger with morgan

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
+import morgan from "morgan";
 
 const router = express.Router();
 
+router.use(morgan(":method :url :status"));
+
 router.get("/", (req: Request, res: Response) => {
-	res.send("api");
+	res.status(200).send("api");
 });
 
 export default router;
