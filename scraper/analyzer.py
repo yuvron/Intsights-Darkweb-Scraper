9e5f2e705bec245080984b065eb4718c5086a6b1
yuvron <yuvr99@gmail.com>
2022-09-13 17:23:42 +0300

    Created analyzer with hard-coded tags

---
+++ scraper/analyzer.py
@@ -0,0 +1,14 @@
+from ctypes.wintypes import tagMSG
+import json
+
+with open("tags.json") as f:
+    tags = json.load(f)
+
+
+def analyze_posts(posts: list[dict]):
+    for post in posts:
+        for tag in tags:
+            for keyword in tags[tag]:
+                if keyword in post["title"].lower() or keyword in post["content"].lower():
+                    post["tags"].append(tag)
+                    break
