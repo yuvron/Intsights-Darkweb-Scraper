508b2eacd175bb6866dde3228904000f613a0229
yuvron <yuvr99@gmail.com>
2022-09-14 15:18:51 +0300

    Implemented all basic endpoints

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
 import pastesRouter from "./pastes";
+import * as db from "../database/db";
 
 const router = express.Router();
 
@@ -12,12 +13,14 @@ router.use("/pastes", pastesRouter);
 
 // Gets all the tags and the amount of pastes which in they appear
 router.get("/tags", (req: Request, res: Response) => {
-	res.status(200).send("get all tags quantified");
+	const tags = db.getTagsByQuantity();
+	res.status(200).json(tags);
 });
 
 // Gets the 5 most active authors
 router.get("/authors/top", (req: Request, res: Response) => {
-	res.status(200).send("get 5 most popular authors");
+	const authors = db.getTopAuthors();
+	res.status(200).json(authors);
 });
 
 export default router;
--- backend/src/routes/pastes.ts
+++ backend/src/routes/pastes.ts
@@ -1,20 +1,24 @@
 import express, { Request, Response } from "express";
+import * as db from "../database/db";
 
 const router = express.Router();
 
 // Gets all the pastes
-router.get("/pastes", (req: Request, res: Response) => {
-	res.status(200).send("get all pastes");
+router.get("/", async (req: Request, res: Response) => {
+	const pastes = await db.getAllPastes();
+	res.status(200).json(pastes);
 });
 
 // Gets the count of all the pastes
-router.get("/pastes/all/count", (req: Request, res: Response) => {
-	res.status(200).send("get count of all pastes");
+router.get("/all/count", async (req: Request, res: Response) => {
+	const count = await db.getAllPastesCount();
+	res.status(200).send(String(count));
 });
 
 // Gets the count of all the pastes from today
-router.get("/pastes/today/count", (req: Request, res: Response) => {
-	res.status(200).send("get count of all pastes from today");
+router.get("/today/count", async (req: Request, res: Response) => {
+	const count = await db.getTodayPastesCount();
+	res.status(200).send(String(count));
 });
 
 export default router;
