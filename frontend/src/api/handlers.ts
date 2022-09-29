70a675f00fc2cafecd1a161023ca3b00a27e87ca
yuvron <yuvr99@gmail.com>
2022-09-29 14:41:41 +0300

    Added a proactive response delay to reach a minimum response time (for a more pleasent ui)

--- frontend/src/api/handlers.ts
+++ frontend/src/api/handlers.ts
@@ -1,8 +1,14 @@
 import { AxiosResponse, AxiosError } from "axios";
 
+const MINIMUM_RESPONSE_TIME = 500;
+
 export async function responseHandler(response: Promise<AxiosResponse>) {
 	try {
+		const pre = performance.now();
 		const { data } = await response;
+		const post = performance.now();
+		const responseTime = post - pre;
+		if (responseTime < MINIMUM_RESPONSE_TIME) await delay(MINIMUM_RESPONSE_TIME - responseTime);
 		return data;
 	} catch (err) {
 		const axiosError = err as AxiosError;
@@ -10,3 +16,5 @@ export async function responseHandler(response: Promise<AxiosResponse>) {
 		else console.log(axiosError.message);
 	}
 }
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
