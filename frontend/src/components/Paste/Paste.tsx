3a41383c582e6c06ba5ec8ac3dcc0f7c46be21df
yuvron <yuvr99@gmail.com>
2022-10-01 21:25:06 +0300

    Made paste content collapsible and animated

--- frontend/src/components/Paste/Paste.tsx
+++ frontend/src/components/Paste/Paste.tsx
@@ -1,13 +1,17 @@
 import IPaste from "../../interfaces/paste";
-import "./Paste.scss";
 import ReactTimeAgo from "react-time-ago";
 import { useRef, useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import "./Paste.scss";
 
 interface PasteProps {
 	paste: IPaste;
 }
 
 const Paste: React.FC<PasteProps> = ({ paste }) => {
+	const [showContent, setShowContent] = useState(false);
+	const contentRef = useRef<HTMLDivElement>(null);
+
 	const { title, content, author, date } = paste;
 
 	return (
@@ -15,10 +19,22 @@ const Paste: React.FC<PasteProps> = ({ paste }) => {
 			<div className="header">
 				<div className="title">{title}</div>
 				<div className="metadata">
-					<span className="author">{author}</span>, <ReactTimeAgo date={new Date(date)} />
+					<span className="author">{author}</span>
+					<span>
+						, <ReactTimeAgo date={new Date(date)} />
+					</span>
+					<span className="expand" onClick={() => setShowContent(!showContent)}>
+						{showContent ? <FaMinus /> : <FaPlus />}
+					</span>
 				</div>
 			</div>
-			<div className="content">{content}</div>
+			<div
+				ref={contentRef}
+				className={`content ${showContent ? "open" : ""}`}
+				style={showContent ? { height: contentRef.current!.scrollHeight + "px" } : { height: "0px" }}
+			>
+				{content}
+			</div>
 		</div>
 	);
 };
