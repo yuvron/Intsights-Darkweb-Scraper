44cd73997da19efab234c0df62054486098ab684
yuvron <yuvr99@gmail.com>
2022-10-02 03:03:42 +0300

    Navbar styling

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,17 +1,21 @@
 import { NavLink } from "react-router-dom";
+import { FaBell } from "react-icons/fa";
 import "./Navbar.scss";
 
 const Navbar: React.FC = () => {
 	return (
 		<nav className="navbar">
 			<div className="links">
-				<NavLink to="/">
+				<NavLink to="/" end>
 					<span>Dashboard</span>
+					<div></div>
 				</NavLink>
 				<NavLink to="/pastes">
 					<span>Pastes</span>
+					<div></div>
 				</NavLink>
 			</div>
+			<FaBell className="notifications" />
 		</nav>
 	);
 };
