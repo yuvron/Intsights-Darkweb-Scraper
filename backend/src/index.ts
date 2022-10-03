50689cc7c95a536a8d19d70369e22ce7c9b124ca
yuvron <yuvr99@gmail.com>
2022-10-04 00:17:46 +0300

    Established socket io server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,32 @@
 import "./config/config";
 import path from "path";
 import express, { Request, Response } from "express";
+import { Server, Socket } from "socket.io";
 import { json } from "body-parser";
 import apiRouter from "./routes/api";
 import * as db from "./database/db";
+import { createServer } from "http";
 
 const PORT = +process.env.PORT;
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket: Socket) => {
+	const connections = [...io.sockets.sockets].length;
+	console.log(`User joined, ${connections} connected`);
+	socket.on("disconnect", () => {
+		const connections = [...io.sockets.sockets].length;
+		console.log(`User left, ${connections} connected`);
+	});
+});
 
 app.use(json());
 
 // Initialization of database and server
 db.connect()
-	.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+	.then(() => server.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
 	.catch((err) => console.log(err.message));
 
 // Routes to api endpoints
