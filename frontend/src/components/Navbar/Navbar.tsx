79a7b7caca064b8fd31ca5daa203ba750f4e4aab
Yuval Ron <yuvr99@gmail.com>
2022-10-10 22:57:35 +0300

    Notifications for new pastes when user connects

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,51 @@
 import { NavLink } from "react-router-dom";
 import { FaBell } from "react-icons/fa";
 import "./Navbar.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useSocket from "../../hooks/useSocket";
+import { IPaste, ICompactPaste } from "../../interfaces/paste";
 
 const Navbar: React.FC = () => {
-	const [notifications, setNotifications] = useState(5);
+	const [notifications, setNotifications] = useState<string[]>([]);
 	const [showNotifications, setShowNotifications] = useState(false);
+
+	const socket = useSocket();
+
+	useEffect(() => {
+		socket.on("new_pastes", (pastes: ICompactPaste[]) => {
+			const newNotifications: string[] = [];
+			const count = pastes.length;
+			if (count > 0) {
+				const countMsg = `${count} new paste${count > 1 ? "s were" : " was"} scraped while you were offline`;
+				newNotifications.push(countMsg);
+			}
+			// TODO: check tags
+			setNotifications((prevNotifications) => [...newNotifications, ...prevNotifications]);
+		});
+		socket.on("scraping_done", (msg: { error: boolean; pastes: ICompactPaste[] }) => {
+			let newNotification = "";
+			if (msg.error) {
+				newNotification = "Scraping process failed";
+			} else {
+				newNotification = "Scraping process succeeded\n";
+				const count = msg.pastes.length;
+				if (count > 0) {
+					newNotification += `${count} new paste${count > 1 ? "s were" : " was"} scraped`;
+				} else {
+					newNotification += "No new pastes were scraped";
+				}
+			}
+			setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
+		});
+		return () => {
+			socket.off("new_pastes");
+			socket.off("scraping_done");
+		};
+	}, []);
 
 	const toggleNotifications = () => {
 		if (showNotifications) {
-			setNotifications(0);
+			setNotifications([]);
 			setShowNotifications(false);
 		} else {
 			setShowNotifications(true);
@@ -30,12 +66,22 @@ const Navbar: React.FC = () => {
 			</div>
 			<div className="notifications">
 				<FaBell onClick={() => toggleNotifications()} />
-				<div className="notifications-counter">{notifications}</div>
+				{notifications.length > 0 && <div className="notifications-counter">{notifications.length}</div>}
 				{showNotifications && (
 					<div className="notifications-container">
-						<div className="notification"></div>
-						<div className="notification"></div>
-						<div className="notification"></div>
+						{notifications.length > 0 ? (
+							<>
+								{notifications.map((notification, index) => {
+									return (
+										<div key={index} className="notification">
+											{notification}
+										</div>
+									);
+								})}
+							</>
+						) : (
+							<div className="notification">There is nothing new</div>
+						)}
 					</div>
 				)}
 			</div>
