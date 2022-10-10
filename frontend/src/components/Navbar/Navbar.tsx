a3d3d999491f9cbf2c70fac4220a757186a8a5d8
Yuval Ron <yuvr99@gmail.com>
2022-10-10 17:15:50 +0300

    Added basic notifications window functionality

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,21 @@
 import { NavLink } from "react-router-dom";
 import { FaBell } from "react-icons/fa";
 import "./Navbar.scss";
+import { useState } from "react";
 
 const Navbar: React.FC = () => {
+	const [notifications, setNotifications] = useState(5);
+	const [showNotifications, setShowNotifications] = useState(false);
+
+	const toggleNotifications = () => {
+		if (showNotifications) {
+			setNotifications(0);
+			setShowNotifications(false);
+		} else {
+			setShowNotifications(true);
+		}
+	};
+
 	return (
 		<nav className="navbar">
 			<div className="links">
@@ -15,7 +28,17 @@ const Navbar: React.FC = () => {
 					<div></div>
 				</NavLink>
 			</div>
-			<FaBell className="notifications" />
+			<div className="notifications">
+				<FaBell onClick={() => toggleNotifications()} />
+				<div className="notifications-counter">{notifications}</div>
+				{showNotifications && (
+					<div className="notifications-container">
+						<div className="notification"></div>
+						<div className="notification"></div>
+						<div className="notification"></div>
+					</div>
+				)}
+			</div>
 		</nav>
 	);
 };
