7975b8db104b473e1a641fab2c7c0a45bad1350a
yuvron <yuvr99@gmail.com>
2022-10-25 12:23:47 +0300

    Extracted notifications logic to a custom hook

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
-import useSocket from "../../hooks/useSocket";
-import { ICompactPaste } from "../../interfaces/paste";
 import { FaBell } from "react-icons/fa";
 import "./Navbar.scss";
+import useNotifications from "../../hooks/useNotifications";
 
 const Navbar: React.FC = () => {
-	const [notifications, setNotifications] = useState<string[]>([]);
 	const [showNotifications, setShowNotifications] = useState(false);
-
-	const socket = useSocket();
+	const [notifications, setNotifications] = useNotifications();
 
 	useEffect(() => {
 		document.addEventListener("click", clickOutsideNotifications);
@@ -18,59 +15,13 @@ const Navbar: React.FC = () => {
 		};
 	}, [showNotifications]);
 
-	useEffect(() => {
-		socket.on("new_pastes", (pastes: ICompactPaste[]) => {
-			if (pastes.length === 0) return;
-			let newNotification = `${pastes.length} new paste${pastes.length > 1 ? "s were" : " was"} scraped while you were offline.`;
-			const watchedTags = localStorage.getItem("watchedTags");
-			if (watchedTags && watchedTags.length > 0) {
-				const receivedTags: { [key: string]: number } = {};
-				pastes.forEach(({ tags }) => {
-					tags.forEach((tag) => {
-						if (receivedTags[tag]) receivedTags[tag]++;
-						else receivedTags[tag] = 1;
-					});
-				});
-				for (const tag in receivedTags) {
-					if (watchedTags.includes(tag)) {
-						newNotification += pastes.length === 1 ? `\nIt's about ${tag}.` : `\n${receivedTags[tag]} of them is about ${tag}.`;
-					}
-				}
-			}
-			setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
-		});
-		socket.on("scraping_done", (msg: { error: boolean; pastes: ICompactPaste[] }) => {
-			let newNotification = "";
-			if (msg.error) {
-				newNotification = "Scraping process failed.";
-			} else {
-				newNotification = "Scraping process succeeded.\n";
-				const count = msg.pastes.length;
-				if (count > 0) {
-					newNotification += `${count} new paste${count > 1 ? "s were" : " was"} scraped.`;
-				} else {
-					newNotification += "No new pastes were scraped.";
-				}
-			}
-			setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
-		});
-		return () => {
-			socket.off("new_pastes");
-			socket.off("scraping_done");
-		};
-	}, []);
-
 	const clickOutsideNotifications = () => {
 		if (showNotifications) toggleNotifications();
 	};
 
 	const toggleNotifications = () => {
-		if (showNotifications) {
-			setNotifications([]);
-			setShowNotifications(false);
-		} else {
-			setShowNotifications(true);
-		}
+		if (showNotifications) setNotifications([]);
+		setShowNotifications((prevShowNotifications) => !prevShowNotifications);
 	};
 
 	return (
@@ -82,6 +33,10 @@ const Navbar: React.FC = () => {
 				</NavLink>
 				<NavLink to="/pastes">
 					<span>Pastes</span>
+					<div></div>
+				</NavLink>
+				<NavLink to="/personal">
+					<span>Personal</span>
 					<div></div>
 				</NavLink>
 			</div>
